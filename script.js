d3dbca80f335ba574adf03ac4af20ba2dcd4e76b
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-04-05 19:29:49 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("menu").classList.toggle('active');
+
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle('active')
+    document.getElementById("menu-bar").classList.toggle('change')
+}
